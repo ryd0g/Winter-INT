e45f643a4b6235d1a563f6ce0c9793920f874e51
Ryan Lee <rjlguitar@gmail.com>
2021-01-07 14:20:48 -0800

    imported footer icons

--- winter-int/src/App.js
+++ winter-int/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import Navigation from './Navigation.js';
+import Footer from './Footer.js';
+
+library.add(fab, faGithub);
 
 function App() {
   return (
     <div className='App'>
       <Navigation />
+      <Footer />
     </div>
   );
 }
