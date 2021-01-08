2dadc85dfe664132e325c7731e76ffcde9e33e45
Ryan Lee <rjlguitar@gmail.com>
2021-01-08 14:31:19 -0800

    added linkedin icons

--- winter-int/src/Footer.js
+++ winter-int/src/Footer.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import './Footer.css';
 
 function Footer() {
@@ -19,6 +20,18 @@ function Footer() {
           href='https://github.com/VenturingGuy'
           target='blank'>
           <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a
+          class='btn btn-social-icon btn-github'
+          href='https://www.linkedin.com/in/ryan-jinwoo-lee/'
+          target='blank'>
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a
+          class='btn btn-social-icon btn-github'
+          href='https://www.linkedin.com/in/luis%2Dv%2D4841a2137/'
+          target='blank'>
+          <FontAwesomeIcon icon={faLinkedin} />
         </a>
       </div>
     </section>
