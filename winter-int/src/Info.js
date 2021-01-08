67b3bbb6a9855bcc6b2c6579b638fe569bb82b67
Ryan Lee <rjlguitar@gmail.com>
2021-01-08 14:38:14 -0800

    added info component

---
+++ winter-int/src/Info.js
@@ -0,0 +1,51 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Info() {
+  return (
+    <section id='features'>
+      <div class='container mb-5 mt-5 pb-5 pt-5'>
+        <h2 class='features-title text-center'>INFO</h2>
+        <div class='row m-5'>
+          <div class='col-lg-4' data-aos='zoom-in' data-aos-duration='1100'>
+            <h3>Title</h3>
+            <p class='lead'>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+          <div class='col-lg-4' data-aos='zoom-in' data-aos-duration='1100'>
+            <h3>Title</h3>
+            <p class='lead'>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+          <div class='col-lg-4' data-aos='zoom-in' data-aos-duration='1100'>
+            <h3>Title</h3>
+            <p class='lead'>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Info;
