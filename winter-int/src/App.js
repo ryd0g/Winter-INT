db73cd82d8c9cc0bedcdce70b43deeb6b711af4e
Ryan Lee <rjlguitar@gmail.com>
2021-01-09 15:22:30 -0800

    added react router paths

--- winter-int/src/App.js
+++ winter-int/src/App.js
@@ -9,18 +9,23 @@ import Main from './Main.js';
 import Info from './Info.js';
 import CTA from './CTA.js';
 import Footer from './Footer.js';
+import Signup from './Signup.js';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 library.add(fab, faGithub);
 
 function App() {
   return (
-    <div className='App'>
-      <Navigation />
-      <Main />
-      <Info />
-      <CTA />
-      <Footer />
-    </div>
+    <Router>
+      <div className='App'>
+        <Navigation />
+        <Main />
+        <Route path='/signup' component={Signup} />
+        <Route path='/info' component={Info} />
+        <CTA />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
