8908b90e53acbc5cf6d77119c0f5dc2166d9614d
Ryan Lee <rjlguitar@gmail.com>
2021-01-09 15:23:02 -0800

    changed cta to remove form

--- winter-int/src/CTA.js
+++ winter-int/src/CTA.js
@@ -1,24 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './CTA.css';
+import { NavLink } from 'react-router-dom';
 
 function CTA() {
   return (
     <section id='CTA'>
       <div class='jumbotron text-center mb-0'>
-        <p class='lead mb-4'>Sign Up for More Infomation</p>
+        <p class='lead mb-4'>Sign Up Today!</p>
         <div class='d-flex justify-content-center'>
           <form class='form-inline'>
-            <input
-              type='text'
-              class='form-control mb-2 mr-sm-2'
-              placeholder='Name'
-            />
-            <input
-              type='text'
-              class='form-control mb-2 mr-sm-2'
-              placeholder='Email'
-            />
-            <input type='date' class='form-control mb-2 mr-sm-2' />
             <label for='message'>Any questions or comments?</label>
             <textarea
               class='form-control mb-2 mr-sm-2'
