78f9c010037f7110cfc3cb1217a416187fe77fd3
Luis <LVentura335@Gmail.com>
2021-01-08 16:10:22 -0800

    carousel and styling

--- winter-int/src/Info.js
+++ winter-int/src/Info.js
@@ -1,12 +1,19 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Info.css'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
-function Info() {
-  return (
-    <section id='features'>
-      <div class='container mb-5 mt-5 pb-5 pt-5'>
-        <h2 class='features-title text-center'>WHAT WE'RE ABOUT</h2>
-        <div class='row m-5'>
-          <div class='col-lg-4' data-aos='zoom-in' data-aos-duration='1100'>
+export default class Info extends React.Component {
+  render() {
+    return (
+      <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={40}
+        totalSlides={3}
+      >
+        <Slider>
+          <Slide index={0}>
             <h3>Who We Are</h3>
             <p class='lead'>
               PortFinder is a website founded by two front-end developers, Ryan and Luis.
@@ -16,17 +23,17 @@ function Info() {
               to the world was one of the main challenges that led to the creation
               of PortFinder.
             </p>
-          </div>
-          <div class='col-lg-4' data-aos='zoom-in' data-aos-duration='1100'>
-            <h3>What We Do</h3>
+            </Slide>
+          <Slide index={1}>
+          <h3>What We Do</h3>
             <p class='lead'>
               On PortFinder, we're concered about being able to connect a prospective
               worker to potential employers in a way that immediately connects the two
               on a professional yet personable level. To that end, we are committed to
               creating an experience that bridges the gap between the two.
             </p>
-          </div>
-          <div class='col-lg-4' data-aos='zoom-in' data-aos-duration='1100'>
+          </Slide>
+          <Slide index={2}>
             <h3>Why It Matters</h3>
             <p class='lead'>
               A portfolio is critical nowadays in the workforce. It isn't just about
@@ -35,11 +42,11 @@ function Info() {
               yourself a far more sought after candidate for a job that could change your life.
               Don't sell yourself short, go above and beyond, and show everyone what you're made of.
             </p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+            </Slide>
+        </Slider>
+        <ButtonBack>Back</ButtonBack>
+        <ButtonNext>Next</ButtonNext>
+      </CarouselProvider>
+    );
+  }
 }
-
-export default Info;
