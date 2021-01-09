49986d704bb6e38f85d858665b81eb182419003b
Ryan Lee <rjlguitar@gmail.com>
2021-01-09 15:23:22 -0800

    added info button

--- winter-int/src/Main.js
+++ winter-int/src/Main.js
@@ -1,12 +1,21 @@
-import './Main.css'
+import './Main.css';
+import { NavLink } from 'react-router-dom';
 
 function Main() {
-    return (
-        <div class="jumbotron">
-            <h1 class="display-3">PortFinder</h1>
-            <h2>Find the right portfolio for the right hire.</h2>
-        </div>
-    )
+  return (
+    <div class='jumbotron'>
+      <h1 class='display-3'>PortFinder</h1>
+      <h2>Find the right portfolio for the right hire.</h2>
+      <button class='btn btn-dark'>
+        <NavLink
+          className='nav-link'
+          activeClassName='nav-link-active'
+          to='/info'>
+          More Info
+        </NavLink>
+      </button>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
