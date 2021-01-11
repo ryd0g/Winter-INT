73f5bbb28c2895ea218621a78f48efcccec3bc0f
Luis <LVentura335@Gmail.com>
2021-01-11 15:33:35 -0800

    added example portfolio

--- winter-int/src/Example.js
+++ winter-int/src/Example.js
@@ -1,9 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Example.css';
 
 function Example() {
   return (
     <section id='portfolio'>
-      <p>hello world</p>
+      <div id='custom-intro' class='jumbotron'>
+        <h2>Roger Smith</h2>
+        <h5>Front End Web Developer</h5>
+      </div>
+      <div class='skills'>
+        <h3>Skills</h3>
+        <ul>
+          Front End Web Development
+          <ul>
+            <li>HTML, CSS, JS - 3 years</li>
+            <li>Bootstrap - 2 years</li>
+            <li>React - 1 year</li>
+          </ul>
+        </ul>
+      </div>
+      <div class='links'>
+        <h4>Links / Contact</h4>
+        <div class="link-display">
+          <a href="#">GitHub</a>
+          <a href="#">LinkedIn</a>
+          <p>Email: RogerTheNegotiator@paradigm.net</p>
+        </div>
+      </div>
     </section>
   );
 }
