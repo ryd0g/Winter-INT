b046e7ee9a28fa4ebcc6a65e2c8b5a0ec9dafca2
Ryan Lee <rjlguitar@gmail.com>
2021-01-11 14:29:37 -0800

    added example page

---
+++ winter-int/src/Example.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Example() {
+  return (
+    <section id='portfolio'>
+      <p>hello world</p>
+    </section>
+  );
+}
+
+export default Example;
