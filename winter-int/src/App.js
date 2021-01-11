98a588bcadc731b09b0eb7cf78dd1201a930d5db
Ryan Lee <rjlguitar@gmail.com>
2021-01-11 14:29:19 -0800

    fixed Main component route

--- winter-int/src/App.js
+++ winter-int/src/App.js
@@ -10,7 +10,8 @@ import Info from './Info.js';
 import CTA from './CTA.js';
 import Footer from './Footer.js';
 import Signup from './Signup.js';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import Example from './Example.js';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 library.add(fab, faGithub);
 
@@ -19,7 +20,8 @@ function App() {
     <Router>
       <div className='App'>
         <Navigation />
-        <Main />
+        <Route exact path='/' component={Main} />
+        <Route path='/example' component={Example} />
         <Route path='/signup' component={Signup} />
         <Route path='/info' component={Info} />
         <CTA />
